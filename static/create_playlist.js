6ece23cdba0ede0d8939af9413bc938669d249fc
Ochoa <cochoa@scu.edu>
2023-11-16 11:44:15 -0800

    Started the create_playlist JS

--- static/create_playlist.js
+++ static/create_playlist.js
@@ -9,7 +9,8 @@
             <label for="{mood}">{mood}</label><br>
 
     on button press event
-        generate playlist
+        find checked checkboxes
+        use those checked to generate playlist
             Options
                 1. find all songs that have a single one of the genres and moods
                 2. find all songs that have all the genres and moods attached
@@ -17,3 +18,91 @@
         maybe try to link to the created playlist in spotify (if there is time)
 */
 
+function createMoodCheckbox(mood) {
+    // create checkbox element
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = mood;
+    checkbox.name = "mood";
+    checkbox.value = mood;
+    
+
+    // create label element and append to checkbox
+    var label = document.createElement("label");
+    label.htmlFor = mood;
+    label.appendChild(document.createTextNode(mood));
+
+    // append to parent
+    var parent = document.getElementsByClassName("mood_list")
+    parent.appendChild(checkbox);
+    parent.appendChild(label);
+}
+
+function createGenreCheckbox(genre) {
+    // create checkbox element
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = genre;
+    checkbox.name = "genre";
+    checkbox.value = genre;
+    
+
+    // create label element and append to checkbox
+    var label = document.createElement("label");
+    label.htmlFor = genre
+    label.appendChild(document.createTextNode(genre));
+
+    // append to parent
+    var parent = document.getElementsByClassName("genre_list");
+    parent.appendChild(checkbox);
+    parent.appendChild(label);
+}
+
+// loop though available moods and create checkboxes
+function loopThroughMoods() {
+    // loop though moods from data base 
+        let mood = ;    // per mood from database make variable
+        createMoodCheckbox(mood)
+    // end loop
+}
+
+// loop though available genres and create checkboxes
+function loopThroughGenres() {
+    // loop though genres from data base 
+        let genre = ;    // per mood from database make variable
+        createMoodCheckbox(genre)
+    // end loop
+}
+
+function checkMoodCheckboxes() {
+    // for each
+        // check if checked
+        // if checked add checkedbox label to array
+}
+
+function checkGenreCheckboxes() {
+    // for each
+            // check if checked
+            // if checked add checkedbox label to array
+}
+
+function checkOptionCheckboxes() {
+    // for all check if checked
+        // if one checked apply
+        // if two checked tell user to only use one or do both 
+    // return outcome
+}
+
+// Loop though and create all available checkboxes for Moods and Genres 
+loopThroughMoods();
+loopThroughGenres();
+
+// Event Listener for Generate Playlist button
+const generateButton = document.querySelector('main button');
+generateButton.addEventListener('click', () =>{
+    checkMoodCheckboxes();
+    checkGenreCheckboxes();
+    var option = checkOptionCheckboxes();
+    createPlaylist(option);
+    // get link to playlist and open link on another page
+});
